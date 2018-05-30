9cca61ad9fad69907bc7c0e3a33ad333edfb2397
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-05-30 17:58:32 +0800

    Add Dagger Init

--- app/src/main/java/com/horsege/kotlinstructure/dagger/AppModule.kt
+++ app/src/main/java/com/horsege/kotlinstructure/dagger/AppModule.kt
@@ -1,4 +1,21 @@
 package com.horsege.kotlinstructure.dagger
 
-class AppModule {
+import android.content.Context
+import com.horsege.kotlinstructure.App
+import com.horsege.kotlinstructure.dagger.qualifier.ApplicationQualifier
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule(private val app: App) {
+
+    @Provides
+    @Singleton
+    fun provideApplication(): App = app
+
+    @Provides
+    @Singleton
+    @ApplicationQualifier
+    fun provideApplicationContext(): Context = app
 }
--- app/src/main/java/com/horsege/kotlinstructure/dagger/qualifier/ApplicationQualifier.kt
+++ app/src/main/java/com/horsege/kotlinstructure/dagger/qualifier/ApplicationQualifier.kt
@@ -1,11 +1,11 @@
-package com.horsege.kotlinstructure.dagger.scope
+package com.horsege.kotlinstructure.dagger.qualifier
 
 import java.lang.annotation.Documented
 import java.lang.annotation.Retention
-import javax.inject.Scope
+import javax.inject.Qualifier
 import java.lang.annotation.RetentionPolicy.RUNTIME
 
-@Scope
+@Qualifier
 @Documented
 @Retention(RUNTIME)
-annotation class ActivityScope
+annotation class ApplicationQualifier
