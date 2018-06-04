bba3b4870a7fe7d98e7a39064bd035fe551a330d
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-06-04 15:02:48 +0800

    Add Module

--- app/src/main/java/com/horsege/kotlinstructure/dagger/DataModule.kt
+++ app/src/main/java/com/horsege/kotlinstructure/dagger/DataModule.kt
@@ -1,13 +1,18 @@
 package com.horsege.kotlinstructure.dagger
 
 import android.content.Context
+import com.birbit.android.jobqueue.JobManager
 import com.horsege.kotlinstructure.dagger.qualifier.ApplicationQualifier
+import com.horsege.kotlinstructure.data.base.CustomEventBus
+import com.horsege.kotlinstructure.data.base.CustomJobManager
+import com.horsege.kotlinstructure.data.base.InteractorExecutorImpl
 import com.horsege.kotlinstructure.data.http.HttpApiService
 import com.horsege.kotlinstructure.data.http.HttpOkHttpClient
 import com.horsege.kotlinstructure.data.http.HttpRetrofit
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import org.greenrobot.eventbus.EventBus
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -26,4 +31,16 @@ class DataModule {
     @Provides
     @Singleton
     fun provideApiService(retorfit: Retrofit): HttpApiService = retorfit.create(HttpApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideEventBus(): EventBus = CustomEventBus.getCustomEventBus()
+
+    @Provides
+    @Singleton
+    fun provideJobManager(@ApplicationQualifier context: Context): JobManager = CustomJobManager.getJobManager(context)
+
+    @Provides
+    @Singleton
+    fun provideInteractorExecutor(jobManager: JobManager, eventBus: EventBus) = InteractorExecutorImpl(jobManager, eventBus)
 }
